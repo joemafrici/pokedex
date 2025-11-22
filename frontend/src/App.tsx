9e6297db89bca53525939d23cd3d166d5b9f3a91
Joe Mafrici <hi@gojoe.dev>
2025-11-21 19:08:34 -0800

    remove some generated react stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -95,23 +95,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Pokedex</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
 
       <h2>Add a new Pokemon</h2>
       <form onSubmit={handleAdd}>
